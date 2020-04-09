a743eb077581f667526ae6a47595936475ee53e1
annal0g <56369862+annal0g@users.noreply.github.com>
2020-04-09 13:47:57 -0400

    Barebones CSS
    Tested on index, other routes TBD

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const app = express();
 const exphbs = require('express-handlebars');
 const cors = require('cors');
 const configRoutes = require('./routes');
+const static = express.static(__dirname + '/public');
 
 app.use(cors());
+app.use('/public', static);
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
